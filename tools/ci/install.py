2d2580bfddd380fb6e3cc468833cad1ada18c6b8
siying233 <952723850@qq.com>
2026-07-09 16:41:55 +0800

    ci: fix No such file

--- tools/ci/install.py
+++ tools/ci/install.py
@@ -76,7 +76,7 @@ def install_resource():
 
 
 def install_chores():
-    for file in ["README.md", "LICENSE", "CONTACT", "requirements.txt"]:
+    for file in ["README.md", "LICENSE","requirements.txt"]:
         shutil.copy2(
             working_dir / file,
             install_path,
